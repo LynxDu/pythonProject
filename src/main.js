f0cf4644435dde7d1be6db3ac70da77bcce1ae77
Lynx <linxdk95@gmail.com>
2022-05-25 20:13:16 +0300

    +

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Vue from 'vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+createApp(App).mount('#app')
